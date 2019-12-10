d6d9ed27aabec7c90f47c875797433501459145e
tomocy <tomocy.dev@gmail.com>
2019-12-10 21:57:45 +0900

    add Profiler
    Profiler represents a profiler of widget rebuilds.

--- lib/rebuild_stats.dart
+++ lib/rebuild_stats.dart
@@ -1,5 +1,41 @@
 library rebuild_stats;
 
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+
+class Profiler {
+  Profiler({
+    this.watchedWidgets = const <String>[],
+  })  : _streamController = StreamController(),
+        _statCounts = Map.fromIterable(
+          watchedWidgets,
+          key: (widget) => widget,
+          value: (widget) => 0,
+        );
+
+  final List<String> watchedWidgets;
+  final StreamController<Stat> _streamController;
+  final Map<String, int> _statCounts;
+
+  void profile() {
+    assert(debugOnRebuildDirtyWidget == null);
+
+    debugOnRebuildDirtyWidget = _onRebuildWidget;
+    _streamController.stream.listen((stat) => _statCounts[stat.widget]++);
+  }
+
+  void _onRebuildWidget(Element element, bool onceBuilt) {
+    final widget = element.widget;
+    if (!watchedWidgets.contains(widget.runtimeType.toString())) {
+      return;
+    }
+
+    _streamController.sink.add(Stat(
+      widget: widget.runtimeType.toString(),
+    ));
+  }
+}
+
 class Stat {
   const Stat({
     this.widget,
